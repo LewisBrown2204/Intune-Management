7f4ac2671715865f9a89a2067fc073fa6643f3cd
LewisBrown2204 <lewis_brown01@hotmail.co.uk>
2023-05-22 19:12:51 +0100

    Custom PSADT

--- Source/Configure_Uninstallation.ps1
+++ Source/Configure_Uninstallation.ps1
@@ -1,33 +1,46 @@
-## Purpose: Uninstall script to check if 'Process' is running, if running run Deploy-application.exe in session 1 with ServiceUI, else run it in Session 0 Silent
+## Configure_Uninstallation.ps1
+## Purpose: Uninstall script to check if 'Process(es)' is running, if running run Deploy-application.exe in session 1 with ServiceUI, else run it in Session 0 Silent
+## TODO - Check if 'Process(es)' is running
 
-# TODO Check if 'Process' is running
-$process_name = "Process"
-$processes = @(Get-Process $process_name -ErrorAction SilentlyContinue)
+$application = ""
+$logFile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\$application Uninstallation_Configuration.log"
+
+# Function to write log messages to the file
+function Write-Log([string]$message) {
+    $timestamp = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+    $logEntry = "$timestamp - $message"
+    Add-Content -Path $logFile -Value $logEntry
+}
+
+$process_names = "", ""
+$processes = @(Get-Process -Name $process_names -ErrorAction SilentlyContinue)
 
 if ($processes.Count -eq 0) {
     Try {
-        Write-Output "$process_name is not running, we can run the Uninstallation in session 0"
-        Start-Process Deploy-Application.exe -Wait -ArgumentList '-DeploymentType "Uninstall" -DeployMode "Silent"'
+        $message = "$process_names are not running, we can run the Uninstallation in session 0"        
+        Write-Log $message
+        Start-Process -FilePath .\Deploy-Application.exe -Wait -ArgumentList '-DeploymentType "Uninstall" -DeployMode "Silent"'
         $exit_code = $LASTEXITCODE
     }
     Catch {
         $ErrorMessage = $_.Exception.Message
-        $ErrorMessage
+        Write-Log $ErrorMessage
         $exit_code = 1
     }
 }
 else {
     Try {
-        Write-Output "$process_name is running, we need to run the Uninstallation in session 1, ServiceUI.exe"
-        Start-Process -FilePath ".\ServiceUI.exe" -ArgumentList "-process:explorer.exe", "Deploy-Application.exe", '-DeploymentType "Uninstall"' -Wait
+        $message = "$process_names are running, we need to run the Uninstallation in session 1 using ServiceUI.exe"        
+        Write-Log $message
+        .\ServiceUI.exe -process:explorer.exe Deploy-Application.exe -DeploymentType "Uninstall"
         $exit_code = $LASTEXITCODE
     }
     Catch {
         $ErrorMessage = $_.Exception.Message
-        $ErrorMessage
+        Write-Log $ErrorMessage
         $exit_code = 1
     }
 }
 
-Write-Output "Exit Code of Uninstallation is: $exit_code"
+Write-Log $message
 exit $exit_code
